673801c308771b311ea27d5c359ad13485c0a0ec
Va Nuwa <vanuwa@gmail.com>
2016-08-14 21:16:27 +0300

    configured template engine pug (jade) for views;

--- app/core/service.js
+++ app/core/service.js
@@ -6,6 +6,7 @@ const Hapi = require('hapi');
 const Inert = require('inert');
 const Good = require('good');
 const Vision = require('vision');
+const pug = require('pug');
 
 class Service {
   constructor (options) {
@@ -29,34 +30,59 @@ class Service {
   _configure (options) {
     const server = this._server;
 
-    server.connection({
-      host: options.host,
-      port: options.port
-    });
+    return new Promise((resolve, reject) => {
+      server.connection({
+        host: options.host,
+        port: options.port
+      });
 
-    if (options.routes) {
-      server.route(options.routes);
-    }
+      if (options.routes) {
+        server.route(options.routes);
+      } else {
+        server.route([{
+          method: 'GET',
+          path: '/',
+          handler: (request, reply) => {
+            reply.view('index');
+          }
+        }]);
+      }
 
-    return server.register([
-      Inert, Vision, {
-        register: Good,
-        options: {
-          reporters: {
-            console: [{
-              module: 'good-squeeze',
-              name: 'Squeeze',
-              args: [{
-                response: '*',
-                log: '*'
-              }]
-            }, {
-              module: 'good-console'
-            }, 'stdout']
+      server.register([
+        Inert, Vision, {
+          register: Good,
+          options: {
+            reporters: {
+              console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                  response: '*',
+                  log: '*'
+                }]
+              }, {
+                module: 'good-console'
+              }, 'stdout']
+            }
           }
         }
-      }
-    ]);
+      ]).then(() => {
+        server.views({
+          engines: {
+            pug
+          },
+          defaultExtension: 'pug',
+          relativeTo: Path.join(__dirname, '../'),
+          path: 'views'
+        });
+
+        resolve(true);
+      }, (error) => {
+        reject(error);
+      }).catch((exception) => {
+        reject(exception);
+      });
+    });
   }
 
   get server () {
