eaa33b62b0c7e20c0f8556cdeb1dc813cfe36ae7
Ivan Kebal <i.kebal@kwebbl.com>
2016-08-16 10:40:43 +0300

    modified output formatting;

--- app/controllers/events_controller.js
+++ app/controllers/events_controller.js
@@ -1,10 +1,12 @@
 /**
  * Created by vanuwa on 8/16/16.
  */
+const util = require('util');
+
 class EventsController {
   create (request, reply) {
     if (request.payload && request.payload.event) {
-      console.log(`[ EventsController ][ POST /events ] payload is ${request.payload.event}`);
+      console.log(`[ user-friendly-api-client ] Event is\r\n${util.inspect(request.payload.event)}\r\n\r\n`);
       reply({ 'ok': true }).code(200);
     } else {
       reply({ msg: 'Bad request payload', code: 400 }).code(400);
