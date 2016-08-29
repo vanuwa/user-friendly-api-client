296a99a96386bec8a11a9aabba1d0ca0984c33c6
Ivan Kebal <i.kebal@kwebbl.com>
2016-08-29 21:31:04 +0300

    implemented create call action;

--- app/controllers/calls_controller.js
+++ app/controllers/calls_controller.js
@@ -1,6 +1,9 @@
 /**
  * Created by ikebal on 29.08.16.
  */
+const util = require('util');
+const Call = require('../models/call_model');
+const Boom = require('boom');
 
 class CallsController {
   index (request, reply) {
@@ -23,8 +26,31 @@ class CallsController {
   }
 
   create (request, reply) {
+    if (request.payload) {
+      const call = buildCallModel(request.payload);
+      console.log(`[ call ][ create ] ${util.inspect(call)}`);
+
+      Call.create(call).then(() => reply.redirect('/calls'), (error) => reply(Boom.badImplementation(error)))
+        .catch((error) => reply(Boom.badImplementation(error)));
+    } else {
+      reply(Boom.badRequest('Bad payload'));
+    }
+
     reply('create a call').code(200);
   }
+}
+
+function buildCallModel (payload) {
+  return {
+    from: {
+      key: payload.call_from_key,
+      value: payload.call_from_value
+    },
+    to: {
+      key: payload.call_to_key,
+      value: payload.call_to_value
+    }
+  };
 }
 
 module.exports = CallsController;
---
+++ app/models/call_model.js
@@ -0,0 +1,59 @@
+/**
+ * Created by ikebal on 29.08.16.
+ */
+const config = require('config').api;
+const ApplicationModel = require('./application_model');
+const request = require('request');
+const util = require('util');
+const auth_token = 'Bearer T5mpEtLYEP8OY97y2m4ItYkHDhsflDPH';
+
+const api_url = `${config.protocol}://${config.host}:${config.port}`;
+
+class CallModel extends ApplicationModel {
+  _defaultModel () {
+    return {
+      organization_id: null,
+      from: {
+        key: '',
+        value: ''
+      },
+      to: {
+        key: '',
+        value: ''
+      }
+    };
+  }
+
+  static create (document) {
+    return new Promise((resolve, reject) => {
+      const uri = `${api_url}/calls`;
+      const options = {
+        method: 'POST',
+        uri,
+        json: true,
+        body: {
+          call: document
+        },
+        headers: {
+          authorization: auth_token
+        }
+      };
+
+      console.log(`[ POST ] ${options.uri}`);
+
+      request(options, (error, response, body) => {
+        console.log(`[ remove ][ Status Code ] ${response && response.statusCode}\r\n`);
+        console.log(`[ remove ][ ERROR ] ${util.inspect(error)}\r\n`);
+        console.log(`[ remove ][ BODY ] ${util.inspect(body)}\r\n`);
+
+        if (response && response.statusCode === 200) {
+          resolve(body);
+        } else {
+          reject(error || body);
+        }
+      });
+    });
+  }
+}
+
+module.exports = CallModel;
